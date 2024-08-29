130cc5e09b509b75cc41e17a37ab2589d3649e4e
Tran Duy Tan (CTV-CNTT) <tantd1@kienlongbank.com>
2024-08-29 19:45:50 +0700

    update

---
+++ src/main/java/com/ojt/klb/MicroservicesCoreBankingApplication.java
@@ -0,0 +1,13 @@
+package com.ojt.klb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCoreBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesCoreBankingApplication.class, args);
+	}
+
+}
