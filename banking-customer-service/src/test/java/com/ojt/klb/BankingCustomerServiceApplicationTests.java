c34790dfe2998ea59d8efdeda647af2ab5dc95b9
Tran Duy Tan (CTV-CNTT) <tantd1@kienlongbank.com>
2024-08-30 08:21:46 +0700

    Initial commit

--- banking-customer-service/src/test/java/com/ojt/klb/BankingCustomerServiceApplicationTests.java
+++ banking-customer-service/src/test/java/com/ojt/klb/BankingCustomerServiceApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroservicesCoreBankingApplicationTests {
+class BankingCustomerServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
